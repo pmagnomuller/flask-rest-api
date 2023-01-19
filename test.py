fde47f502730c705d302a964adab85360365a167
Pedro Magno Müller <pm.mueller@pragmaticindustries.de>
2023-01-19 13:50:36 +0100

    initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+response = requests.put(BASE + "video/1", {"likes": 10, "name": "Tim", "views": 100})
+print(response.json())
+input()
+response = requests.get(BASE + "video/1")
+print(response.json())
